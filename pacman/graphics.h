c8d1be2744931e6f859da69a01c209976dd8e388
Andrea <s333224@studenti.polito.it>
2024-12-17 16:20:04 +0100

    Formatting

--- pacman/graphics.h
+++ pacman/graphics.h
@@ -8,8 +8,10 @@
 
 void draw_game_info_labels();
 void draw_game_info_values(Game* game);
+
 void draw_tile(uint16_t row, uint16_t col, Tile* tile);
 void draw_map(Tile map[MAP_HEIGHT][MAP_LENGTH]);
+
 void draw_pacman(uint16_t row, uint16_t col);
 
 #endif // GRAPHICS_H *
