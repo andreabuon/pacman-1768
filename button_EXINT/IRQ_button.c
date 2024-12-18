6b73b84846e1d5f45ce158d94286b1ebde38f8bb
Andrea <s333224@studenti.polito.it>
2024-12-18 12:11:12 +0100

    Added mode switching on INT0 - NO DEBOUNCING

--- button_EXINT/IRQ_button.c
+++ button_EXINT/IRQ_button.c
@@ -1,11 +1,27 @@
 #include "button.h"
 #include "../RIT/RIT.h"
 
+#include "../pacman/pacman.h"
+
 extern int pressed_button_0;
 extern int pressed_button_1;
 extern int pressed_button_2;
 
+extern Game game;
+
 void EINT0_IRQHandler(void){
+	if(game.state == PLAYING){
+		pause_game(&game);
+	}	else if (game.state == PAUSED || game.state == READY){
+		start_game(&game);
+	} else if (game.state == WON || game.state == GAME_OVER){
+		// Do nothing
+	}
+	// Remove interrput bit
+    LPC_SC->EXTINT |= (1 << 0);
+	
+	/* Template
+	
 	// Variable set to 1	
     pressed_button_0 = 1;
 	// Disable interrupt
@@ -15,6 +31,8 @@ void EINT0_IRQHandler(void){
 	// Change pin selection so that the button 
 	// does not work any longer. 
     LPC_PINCON->PINSEL4 &= ~(1 << 20);
+	
+	*/
 }
 
 void EINT1_IRQHandler(void){
