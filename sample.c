47b78c52b6a83a215ccd0dee2b0e910e891d32d3
Andrea <s333224@studenti.polito.it>
2024-12-23 11:39:15 +0100

    Removed comments and removed unused buttons init

--- sample.c
+++ sample.c
@@ -30,12 +30,7 @@ int main (void) {
 	joystick_init();
 	LCD_Initialization();
 	
-	// BUTTON_init:
-	//		1: which button
-	//		2: priority of the associated interrupt
 	BUTTON_init(BUTTON_0, PRIO_3);
-	BUTTON_init(BUTTON_1, PRIO_3);
-	BUTTON_init(BUTTON_2, PRIO_3);
 	
 	// RIT WORKS WITH CLOCK = 100MHZ
 	// ONE INTERRUPT EVERY 50ms
@@ -49,22 +44,7 @@ int main (void) {
 	// 		4: mr1
 	//		5: configuration of MR0 (interrupt, reset and stop) ( | of TIMER_INTERRUPT_MR, TIMER_RESET_MR, TIMER_STOP_MR)
 	//		6: configuration of MR1 (interrupt, reset and stop) ( | of TIMER_INTERRUPT_MR, TIMER_RESET_MR, TIMER_STOP_MR)
-	
-	// TIMER_0
-	// f_timer_hz    = 25000000 Hz = 25MHz
-	// prescaler     = 
-	// MR            = 
-	// f_counter_hz  = f_timer_hz / (prescaler + 1) = 
-	// T_timer       = MR / f_counter_hz = 
-	// MR = T_timer * f_timer_hz / (prescaler + 1) = 
-	
-	// USE TIM_MS_TO_TICKS_SIMPLE for default PR (=0) and f_timer_hz values
-	// use TIM_MS_TO_TICKS for custom PR and f_timer_hz values
-	// max ms value is 170000 (2min 50s) => TIM_MS_TO_TICKS_SIMPLE(170000)=2^31-1
 	*/
-	// init_timer_simplified(TIMER_0, prescale, mr0, mr1, conf_mr0, conf_mr1); enable_timer(TIMER_0, PRIO_3);
-	
-	// init_timer_pwm(TIMER_0, 0.5, 170000U); enable_timer(TIMER_0, PRIO_3);
 	
 	// Game Time countdown update
 	init_timer_simplified(TIMER_0, 0, TIM_MS_TO_TICKS_SIMPLE(1000), 0, TIMER_INTERRUPT_MR | TIMER_RESET_MR, 0);
@@ -75,10 +55,8 @@ int main (void) {
 	// power control register
 	LPC_SC->PCON |= 0x1;			// PM0=1
 	LPC_SC->PCON &= 0xFFFFFFFFD;	// PM1=0
-	//execution of wfi or wfe assembly enters Power-Down mode when SLEEPDEEP is on
 	
 	LCD_Clear(Black);
-	//GUI_Text(0, 280, (uint8_t *) " touch here : 1 sec to clear  ", Red, White);
 
 	new_game(&game);
 	
@@ -96,48 +74,6 @@ int main (void) {
 			__ASM("wfi");
 	}
 }
-/*
-		// Joystick cmd, flags set at first edge
-		if(joystick_flag & FLAG_JOYSTICK_UP) {
-			joystick_flag &= ~FLAG_JOYSTICK_UP;
-		}
-		*/
-		/*
-		if(joystick_flag & FLAG_JOYSTICK_DOWN) {
-			joystick_flag &= ~FLAG_JOYSTICK_DOWN;
-		}
-		*/
-		/*
-		if(joystick_flag & FLAG_JOYSTICK_LEFT) {
-			joystick_flag &= ~FLAG_JOYSTICK_LEFT;
-		}
-		*/
-		/*
-		if(joystick_flag & FLAG_JOYSTICK_RIGHT) {
-			joystick_flag &= ~FLAG_JOYSTICK_RIGHT;
-		}
-		*/
-		/*
-		if(joystick_flag & FLAG_JOYSTICK_SELECT) {
-			joystick_flag &= ~FLAG_JOYSTICK_SELECT;
-		}
-		*/
-		// Buttons, flags set when released
-		/*
-		if(btn_flag & FLAG_BUTTON_0) {
-			btn_flag &= ~FLAG_BUTTON_0;
-		}
-		*/
-		/*
-		if(btn_flag & FLAG_BUTTON_1) {
-			btn_flag &= ~FLAG_BUTTON_1;
-		}
-		*/
-		/*
-		if(btn_flag & FLAG_BUTTON_2) {
-			btn_flag &= ~FLAG_BUTTON_2;
-		}
-		*/
 
 /* Initialize SysTick using CMSIS Core_CM4 function */
 static void InitSysTick(void){
