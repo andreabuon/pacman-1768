52750453c701553fd558a5d99e6136f1cca53b7d
Andrea Buonaurio <s333224@studenti.polito.it>
2025-01-11 12:07:54 +0100

    added song, added timer 2,3 & RIT IRQ handlers

--- RIT/IRQ_RIT.c
+++ RIT/IRQ_RIT.c
@@ -6,6 +6,7 @@
 #include "../button_EXINT/button.h"
 
 #include "../pacman/pacman.h"
+#include "../pacman/music.h"
 
 volatile uint32_t pressed_button_0 = 0;
 
@@ -14,6 +15,68 @@ uint32_t pressed_joystick_left   = 0;
 uint32_t pressed_joystick_right  = 0;
 uint32_t pressed_joystick_down   = 0;
 uint32_t pressed_joystick_select = 0;
+
+#define UPTICKS 1
+NOTE song[] = 
+{
+		// 1
+	{NOTE_D3, time_16th},
+	{NOTE_D3, time_16th},
+	{NOTE_D4, time_8th},
+	{NOTE_A3, time_8th},
+	{REST, time_16th},
+	{NOTE_GS3, time_16th},
+	{REST, time_16th},
+	{NOTE_G3, time_8th},
+	{NOTE_F3, time_16th * 2},
+	{NOTE_D3, time_16th},
+	{NOTE_F3, time_16th},
+	{NOTE_G3, time_16th},
+
+		// 2
+	{NOTE_C3, time_16th},
+	{NOTE_C3, time_16th},
+	{NOTE_D4, time_8th},
+	{NOTE_A3, time_8th},
+	{REST, time_16th},
+	{NOTE_GS3, time_16th},
+	{REST, time_16th},
+	{NOTE_G3, time_8th},
+	{NOTE_F3, time_16th * 2},
+	{NOTE_D3, time_16th},
+	{NOTE_F3, time_16th},
+	{NOTE_G3, time_16th},
+
+		// 3
+	{NOTE_C3B, time_16th},
+	{NOTE_C3B, time_16th},
+	{NOTE_D4, time_8th},
+	{NOTE_A3, time_8th},
+	{REST, time_16th},
+	{NOTE_GS3, time_16th},
+	{REST, time_16th},
+	{NOTE_G3, time_8th},
+	{NOTE_F3, time_16th * 2},
+	{NOTE_D3, time_16th},
+	{NOTE_F3, time_16th},
+	{NOTE_G3, time_16th},
+
+		// 4
+	{NOTE_GS2, time_16th},
+	{NOTE_GS2, time_16th},
+	{NOTE_D4, time_8th},
+	{NOTE_A3, time_8th},
+	{REST, time_16th},
+	{NOTE_GS3, time_16th},
+	{REST, time_16th},
+	{NOTE_G3, time_8th},
+	{NOTE_F3, time_16th * 2},
+	{NOTE_D3, time_16th},
+	{NOTE_F3, time_16th},
+	{NOTE_G3, time_16th},
+
+		// 5 (remaining part of the song)
+};
 
 void RIT_IRQHandler(void){	
 	
@@ -92,6 +155,23 @@ void RIT_IRQHandler(void){
 				toggle_game_state(&game);
 			}
 		}
+	}
+	
+	static int currentNote = 0;
+	static int ticks = 0;
+	if(!isNotePlaying())
+	{
+		++ticks;
+		if(ticks == UPTICKS)
+		{
+			ticks = 0;
+			playNote(song[currentNote++]);
+		}
+	}
+	
+	if(currentNote == (sizeof(song) / sizeof(song[0])))
+	{
+		disable_RIT();
 	}
 	
 	LPC_RIT->RICTRL |= 0x1;
--- timer/IRQ_timer.c
+++ timer/IRQ_timer.c
@@ -43,12 +43,32 @@ void TIMER1_IRQHandler (void){
 	return;
 }
 
+uint16_t SinTable[45] =
+{
+    410, 467, 523, 576, 627, 673, 714, 749, 778,
+    799, 813, 819, 817, 807, 789, 764, 732, 694, 
+    650, 602, 550, 495, 438, 381, 324, 270, 217,
+    169, 125, 87 , 55 , 30 , 12 , 2  , 0  , 6  ,   
+    20 , 41 , 70 , 105, 146, 193, 243, 297, 353
+};
 
 void TIMER2_IRQHandler(void){
 	uint8_t irq_source = LPC_TIM2->IR;
 	
 	if(irq_source & IR_MR0) { // mr0
 		//powerpills_timer_tick(&game);
+		
+		static int sineticks=0;
+		/* DAC management */	
+		static int currentValue; 
+		currentValue = SinTable[sineticks];
+		currentValue -= 410;
+		currentValue /= 1;
+		currentValue += 410;
+		LPC_DAC->DACR = currentValue <<6;
+		sineticks++;
+		if(sineticks==45) sineticks=0;
+		
 	} else if(irq_source & IR_MR1) { // mr1
 		
 	} else if(irq_source & IR_MR2) { // mr2
@@ -65,7 +85,7 @@ void TIMER3_IRQHandler (void){
 	uint8_t irq_source = LPC_TIM3->IR;
 	
 	if(irq_source & IR_MR0) { // mr0
-		
+		disable_timer(2);
 	} else if(irq_source & IR_MR1) { // mr1
 		
 	} else if(irq_source & IR_MR2) { // mr2
