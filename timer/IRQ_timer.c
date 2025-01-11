0e838ae302d20721a3ce0319414eb8127fe47d4b
Andrea Buonaurio <s333224@studenti.polito.it>
2025-01-11 12:46:05 +0100

    commented out probably useless dacr values conversions

--- timer/IRQ_timer.c
+++ timer/IRQ_timer.c
@@ -62,9 +62,11 @@ void TIMER2_IRQHandler(void){
 		/* DAC management */	
 		static int currentValue; 
 		currentValue = SinTable[sineticks];
+		/*
 		currentValue -= 410;
 		currentValue /= 1;
 		currentValue += 410;
+		*/
 		LPC_DAC->DACR = currentValue <<6;
 		sineticks++;
 		if(sineticks==45) sineticks=0;
