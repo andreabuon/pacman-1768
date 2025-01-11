88f94b30d1fbc78ab3d506a7d315b8d5d167a5dc
Andrea Buonaurio <s333224@studenti.polito.it>
2025-01-11 12:05:09 +0100

    adapted music functions to template's timer functions

--- pacman/music.c
+++ pacman/music.c
@@ -1,21 +1,22 @@
 #include "music.h"
 
 #include "../timer/timer.h"
+#include "pacman.h"
 
 void playNote(NOTE note)
 {
 	if(note.freq != REST)
 	{
-		reset_timer(0);
-		init_timer(0, note.freq*AMPLIFIER*VOLUME);
-		enable_timer(0);
+		reset_timer(TIMER_2);
+		init_timer_simplified(TIMER_2, 1, note.freq*AMPLIFIER*VOLUME, 0, 0, 0);
+		enable_timer(TIMER_2, 0);
 	}
-	reset_timer(1);
-	init_timer(1, note.duration);
-	enable_timer(1);
+	reset_timer(TIMER_3);
+	init_timer_simplified(TIMER_3, 1, note.duration, 0, 0, 0);
+	enable_timer(TIMER_3, 0);
 }
 
 BOOL isNotePlaying(void)
 {
-	return ((LPC_TIM0->TCR != 0) || (LPC_TIM1->TCR != 0));
+	return ((LPC_TIM2->TCR != 0) || (LPC_TIM3->TCR != 0));
 }
