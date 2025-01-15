ada8f3c9e7ed996d714a92ac0272a09581bd514a
Andrea Buonaurio <s333224@studenti.polito.it>
2025-01-15 11:28:33 +0100

    moved songs into a dedicated file and removed song ticks

--- RIT/IRQ_RIT.c
+++ RIT/IRQ_RIT.c
@@ -7,6 +7,7 @@
 
 #include "../pacman/pacman.h"
 #include "../pacman/music.h"
+#include "../pacman/songs.h"
 
 volatile uint32_t pressed_button_0 = 0;
 
@@ -15,145 +16,6 @@ uint32_t pressed_joystick_left   = 0;
 uint32_t pressed_joystick_right  = 0;
 uint32_t pressed_joystick_down   = 0;
 uint32_t pressed_joystick_select = 0;
-
-#define UPTICKS 1
-NOTE megalovania[] = 
-{
-		// 1
-	{NOTE_D3, time_16th},
-	{NOTE_D3, time_16th},
-	{NOTE_D4, time_8th},
-	{NOTE_A3, time_8th},
-	{REST, time_16th},
-	{NOTE_GS3, time_16th},
-	{REST, time_16th},
-	{NOTE_G3, time_8th},
-	{NOTE_F3, time_16th * 2},
-	{NOTE_D3, time_16th},
-	{NOTE_F3, time_16th},
-	{NOTE_G3, time_16th},
-
-		// 2
-	{NOTE_C3, time_16th},
-	{NOTE_C3, time_16th},
-	{NOTE_D4, time_8th},
-	{NOTE_A3, time_8th},
-	{REST, time_16th},
-	{NOTE_GS3, time_16th},
-	{REST, time_16th},
-	{NOTE_G3, time_8th},
-	{NOTE_F3, time_16th * 2},
-	{NOTE_D3, time_16th},
-	{NOTE_F3, time_16th},
-	{NOTE_G3, time_16th},
-
-		// 3
-	{NOTE_C3B, time_16th},
-	{NOTE_C3B, time_16th},
-	{NOTE_D4, time_8th},
-	{NOTE_A3, time_8th},
-	{REST, time_16th},
-	{NOTE_GS3, time_16th},
-	{REST, time_16th},
-	{NOTE_G3, time_8th},
-	{NOTE_F3, time_16th * 2},
-	{NOTE_D3, time_16th},
-	{NOTE_F3, time_16th},
-	{NOTE_G3, time_16th},
-
-		// 4
-	{NOTE_GS2, time_16th},
-	{NOTE_GS2, time_16th},
-	{NOTE_D4, time_8th},
-	{NOTE_A3, time_8th},
-	{REST, time_16th},
-	{NOTE_GS3, time_16th},
-	{REST, time_16th},
-	{NOTE_G3, time_8th},
-	{NOTE_F3, time_16th * 2},
-	{NOTE_D3, time_16th},
-	{NOTE_F3, time_16th},
-	{NOTE_G3, time_16th},
-
-		// 5 (remaining part of the song)
-};
-
-NOTE pacman_theme[] = 
-{
-	// 1
-	{NOTE_B4, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_C5, time_16th},
-	{NOTE_C6, time_16th},
-	{NOTE_G5, time_16th},
-	{NOTE_E5, time_16th},
-	{NOTE_C6, time_16th},
-	{NOTE_G5, time_16th},
-	{NOTE_E5, time_16th},
-
-	// 2
-	{NOTE_B4, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_D5, time_16th},
-	{NOTE_D6, time_16th},
-	{NOTE_A5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_D6, time_16th},
-	{NOTE_A5, time_16th},
-	{NOTE_FS5, time_16th},
-
-	// 3
-	{NOTE_B4, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_E5, time_16th},
-	{NOTE_E6, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_GS5, time_16th},
-	{NOTE_E6, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_GS5, time_16th},
-
-	// 4
-	{NOTE_B4, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_F5, time_16th},
-	{NOTE_F6, time_16th},
-	{NOTE_CS6, time_16th},
-	{NOTE_A5, time_16th},
-	{NOTE_F6, time_16th},
-	{NOTE_CS6, time_16th},
-	{NOTE_A5, time_16th},
-
-	// Ending
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_B5, time_16th},
-	{NOTE_FS5, time_16th},
-	{NOTE_DS5, time_16th},
-	{NOTE_B4, time_8th},
-	{REST, time_8th}
-};
 
 void RIT_IRQHandler(void){	
 	
@@ -235,15 +97,9 @@ void RIT_IRQHandler(void){
 	}
 	
 	static int currentNote = 0;
-	static int ticks = 0;
 	if(!isNotePlaying())
 	{
-		++ticks;
-		if(ticks == UPTICKS)
-		{
-			ticks = 0;
-			playNote(pacman_theme[currentNote++]);
-		}
+		playNote(pacman_theme[currentNote++]);
 	}
 	
 	if(currentNote == (sizeof(pacman_theme) / sizeof(pacman_theme[0])))
