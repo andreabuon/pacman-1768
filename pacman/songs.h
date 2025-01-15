04fa7e3149c3e59f38244792dc5338d90bdbbde5
Andrea Buonaurio <s333224@studenti.polito.it>
2025-01-15 11:54:52 +0100

    fix: song notes durations

--- pacman/songs.h
+++ pacman/songs.h
@@ -65,20 +65,19 @@ NOTE pacman_theme[] =
 {
     // Measure 1
     {NOTE_B4, time_16th}, {NOTE_B5, time_16th}, 
-    {NOTE_FS5, time_16th}, {NOTE_DS5, time_16th},
+    {NOTE_FS5, time_16th}, {NOTE_DS5, time_16th}, 
     {NOTE_B5, time_32nd}, {NOTE_FS5, time_16th_dotted}, 
     {NOTE_DS5, time_8th}, {NOTE_C5, time_16th},
     {NOTE_C6, time_16th}, {NOTE_G6, time_16th}, 
     {NOTE_E6, time_16th}, {NOTE_C6, time_32nd}, 
     {NOTE_G6, time_16th_dotted}, {NOTE_E6, time_8th},
 
+    // Measure 2
     {NOTE_B4, time_16th}, {NOTE_B5, time_16th}, 
-    {NOTE_FS5, time_16th}, {NOTE_DS5, time_16th},
+    {NOTE_FS5, time_16th}, {NOTE_DS5, time_16th}, 
     {NOTE_B5, time_32nd}, {NOTE_FS5, time_16th_dotted}, 
-    {NOTE_DS5, time_8th}, 
-
-		{NOTE_DS5, time_32nd}, 
-    {NOTE_E5, time_32nd}, {NOTE_F5, time_32nd},
+    {NOTE_DS5, time_8th}, {NOTE_DS5, time_32nd}, 
+    {NOTE_E5, time_32nd}, {NOTE_F5, time_32nd}, 
     {NOTE_F5, time_32nd}, {NOTE_FS5, time_32nd}, 
     {NOTE_G5, time_32nd}, {NOTE_G5, time_32nd}, 
     {NOTE_GS5, time_32nd}, {NOTE_A5, time_16th}, 
