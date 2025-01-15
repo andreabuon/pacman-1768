c04116db40982390473f6651c68807c6eb94728a
Andrea Buonaurio <s333224@studenti.polito.it>
2025-01-15 10:51:31 +0100

    feat: added first version of Pacman theme

--- RIT/IRQ_RIT.c
+++ RIT/IRQ_RIT.c
@@ -17,7 +17,7 @@ uint32_t pressed_joystick_down   = 0;
 uint32_t pressed_joystick_select = 0;
 
 #define UPTICKS 1
-NOTE song[] = 
+NOTE megalovania[] = 
 {
 		// 1
 	{NOTE_D3, time_16th},
@@ -76,6 +76,83 @@ NOTE song[] =
 	{NOTE_G3, time_16th},
 
 		// 5 (remaining part of the song)
+};
+
+NOTE pacman_theme[] = 
+{
+	// 1
+	{NOTE_B4, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_C5, time_16th},
+	{NOTE_C6, time_16th},
+	{NOTE_G5, time_16th},
+	{NOTE_E5, time_16th},
+	{NOTE_C6, time_16th},
+	{NOTE_G5, time_16th},
+	{NOTE_E5, time_16th},
+
+	// 2
+	{NOTE_B4, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_D5, time_16th},
+	{NOTE_D6, time_16th},
+	{NOTE_A5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_D6, time_16th},
+	{NOTE_A5, time_16th},
+	{NOTE_FS5, time_16th},
+
+	// 3
+	{NOTE_B4, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_E5, time_16th},
+	{NOTE_E6, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_GS5, time_16th},
+	{NOTE_E6, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_GS5, time_16th},
+
+	// 4
+	{NOTE_B4, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_F5, time_16th},
+	{NOTE_F6, time_16th},
+	{NOTE_CS6, time_16th},
+	{NOTE_A5, time_16th},
+	{NOTE_F6, time_16th},
+	{NOTE_CS6, time_16th},
+	{NOTE_A5, time_16th},
+
+	// Ending
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_B5, time_16th},
+	{NOTE_FS5, time_16th},
+	{NOTE_DS5, time_16th},
+	{NOTE_B4, time_8th},
+	{REST, time_8th}
 };
 
 void RIT_IRQHandler(void){	
@@ -165,11 +242,11 @@ void RIT_IRQHandler(void){
 		if(ticks == UPTICKS)
 		{
 			ticks = 0;
-			playNote(song[currentNote++]);
+			playNote(pacman_theme[currentNote++]);
 		}
 	}
 	
-	if(currentNote == (sizeof(song) / sizeof(song[0])))
+	if(currentNote == (sizeof(pacman_theme) / sizeof(pacman_theme[0])))
 	{
 		currentNote = 0;
 	}
